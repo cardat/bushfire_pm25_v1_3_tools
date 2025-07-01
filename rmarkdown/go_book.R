b952dcb9e88f62655e424da36e2328efe8e1366a
cassandrayuen <yuen.cassandra.i@gmail.com>
2025-07-01 13:33:50 +1000

    upload docs

--- rmarkdown/go_book.R
+++ rmarkdown/go_book.R
@@ -2,10 +2,13 @@ library(bookdown)
 source("rmarkdown/config.R")
 # render_book( "index.Rmd", pdf_book())
 
-render_book("rmarkdown", bookdown::gitbook(split_by = "chapter", self_contained = FALSE, config = list(sharing = NULL, toc = list(collapse = "section"))) 
-            )
-
 unlink("docs", recursive = TRUE)
-file.rename("rmarkdown/_book/", "docs/")
+
+render_book("rmarkdown", bookdown::gitbook(split_by = "chapter", 
+                                           self_contained = FALSE, 
+                                           config = list(sharing = NULL, 
+                                                         toc = list(collapse = "section"))),
+            output_dir = "../docs" # relative to source folder
+            )
 
 browseURL("docs/index.html")
